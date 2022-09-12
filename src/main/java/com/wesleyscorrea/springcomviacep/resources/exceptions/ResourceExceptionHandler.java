c141ee4d71a5e63c2ea72ce9145ad38c802ba268
unknown <Wesleyscorrea@hotmail.com>
2022-09-12 02:24:20 -0300

    Database error

--- src/main/java/com/wesleyscorrea/springcomviacep/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/wesleyscorrea/springcomviacep/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.wesleyscorrea.springcomviacep.resources.exceptions;
 
+import com.wesleyscorrea.springcomviacep.services.exceptions.DataBaseException;
 import com.wesleyscorrea.springcomviacep.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,15 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resourse not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> dataBase(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Data base error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(status).body(err);
